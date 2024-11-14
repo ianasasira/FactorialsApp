b6e205a0a9b903d5ebf6c06e3e4f3d89f632961d
ian asasira <you@example.com>
2024-11-14 16:39:54 +0300

    failed to return value for return_next_no() consequent

--- factorials.c
+++ factorials.c
@@ -2,6 +2,7 @@
 
 int factorial(unsigned int num, int num_break_array[]);
 int num_break(unsigned int num, int num_break_array[]);
+int return_next_no(unsigned int num, int num_break_array[], int j);
 int main()
 {
     // program to calculate the factorial of a number
@@ -28,9 +29,17 @@ int num_break(unsigned int num, int num_break_array[])
     {
         num_break_array[i] = i;
     }
-    for (int i = 1; i <= num; i++)
+    for (int j = 1; j <= num; j += 2)
     {
-        unsigned new_num = num_break_array[i] * num_break_array[i + 1];
-        printf("Array %d", num_break_array[i]);
+        unsigned new_num = num_break_array[j] * return_next_no(num, num_break_array, j);
+
+        printf("Array %d", new_num);
+    }
+}
+int return_next_no(unsigned int num, int num_break_array[], int j)
+{
+    for (int i = j; i <= num; i++)
+    {
+        return num_break_array[j + 1];
     }
 }
